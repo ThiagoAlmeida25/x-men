29b40516942061422ce0252f3f80c03b396433e1
ThiagoAlmeida25 <133608527+ThiagoAlmeida25@users.noreply.github.com>
2023-12-03 21:05:56 -0300

    Add files via upload

---
+++ src/js/index1.js
@@ -0,0 +1,17 @@
+/* esse é um comentário
+*/
+
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach(personagem => {
+    personagem.addEventListener('mouseenter', () => {
+        personagem.classList.add('selecionado');
+
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+
+        personagem.classList.add('selecioado');
+
+    })
+})
